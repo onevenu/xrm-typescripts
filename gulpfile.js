715c88e8ae9c0efbade5883f806123cbfbdfe591
Vennu Gopal <vennu.gopal@accenture.com>
2017-10-20 00:39:57 +0530

    Configured gulp for typescripts and added typings for xrm

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var concat = require('gulp-concat');
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task('default', function () {
+    debugger;
+
+    gulp.task("default", function () {
+        return tsProject.src()
+            .pipe(concat('scripts.ts'))
+            .pipe(tsProject())
+
+            .pipe(gulp.dest("dist"));
+    });
+
+
+});
+gulp.task('watch', ['default'], function () {
+    gulp.watch('src/*.ts', []);
+});
