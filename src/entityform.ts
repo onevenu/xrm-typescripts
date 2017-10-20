cf674d6e4a019d4daac62864bc3c02864b6f9901
Vennu Gopal <vennu.gopal@accenture.com>
2017-10-20 16:44:23 +0530

    events added

---
+++ src/entityform.ts
@@ -0,0 +1,12 @@
+/// <reference path="../typings/xrm/xrm.d.ts"/>
+class EntityForm {
+    static onLoad(): void {
+
+    }
+    static onSave(): void {
+
+    }
+    static onChange(): void {
+
+    }
+}
