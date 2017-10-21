99a57a51f891bf07778e30b5ed931325a87000c4
Vennu Gopal <vennu.gopal@accenture.com>
2017-10-21 15:06:42 +0530

    changed inputs in doUserHasRole function

--- src/commonmethods.ts
+++ src/commonmethods.ts
@@ -1,6 +1,10 @@
 /// <reference path="../typings/xrm/xrm.d.ts"/>
 class CommonMethods {
-    static doUserHasRole(inputRole: string, buID: string): boolean {
+    static doUserHasRole(inputRole: string): boolean {
+
+        let currentUserId: string = Xrm.Page.context.getUserId().replace(/[{}]/g, '');
+        let currentUser: any = Service.Retrieve("systemusers",currentUserId,"_businessunitid_value,systemuserid");
+        let currentUserBuId: string = currentUser._businessunitid_value;
 
         let userCurrentRoles: string[] = Xrm.Page.context.getUserRoles();
 
@@ -8,7 +12,7 @@ class CommonMethods {
 
         let role: any = Service.RetrieveMultiple("roles",
             "_businessunitid_value,name,roleid",
-            "name eq '" + inputRole + "' and  _businessunitid_value eq " + buID);
+            "name eq '" + inputRole + "' and  _businessunitid_value eq " + currentUserBuId);
 
         let inputRoleId: string = role != undefined && role.length > 0 ? role[0].roleid : "";
         let flag: number = 0;
